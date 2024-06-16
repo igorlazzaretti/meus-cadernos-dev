9dd4c60afab6f6784a922c5ecf10ec01960400cc
Igor Dossin Lazzaretti <134664486+igorlazzaretti@users.noreply.github.com>
2024-06-16 17:09:14 -0300

    commit
    Creating the index of my notes

--- script/script.js
+++ script/script.js
@@ -1,16 +1,29 @@
+// Loading
+var i = setInterval(function () {
+    clearInterval(i);
+
+    // The desired code is only this:
+    document.getElementById("loading").style.display = "none";
+    document.getElementById("content").style.display = "block";
+   
+}, 1500);
+
+
 // script.js
+        // Seleciona todos os botões de toggle e os elementos de conteúdo
+        const toggleButtons = document.querySelectorAll(".toggle-button");
+        const contents = document.querySelectorAll(".toggle-content");
 
-document.addEventListener('DOMContentLoaded', () => {
-    const toggleButton = document.querySelector('.toggle-button');
-    const toggleContent = document.querySelector('.toggle-content');
-
-    toggleButton.addEventListener('click', () => {
-        if (toggleContent.style.display === 'none' || toggleContent.style.display === '') {
-            toggleContent.style.display = 'block';
-            toggleButton.textContent = 'Recolher';
-        } else {
-            toggleContent.style.display = 'none';
-            toggleButton.textContent = 'Mostrar';
-        }
-    });
-});
+        // Adiciona um evento de clique para cada botão
+        toggleButtons.forEach((button, index) => {
+            button.addEventListener("click", () => {
+                const content = contents[index];
+                if (content.style.display === "none") {
+                    content.style.display = "block";
+                    button.textContent = `Esconder`;
+                } else {
+                    content.style.display = "none";
+                    button.textContent = `Mostrar`;
+                }
+            });
+        });
